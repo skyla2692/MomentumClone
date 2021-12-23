baa6342ac182bf8941bf5d0540b9908d5ae88226
skylalee <reloxo99@gmail.com>
2021-12-23 20:58:13 +0900

    User info && clock function ready

---
+++ src/js/clock.js
@@ -0,0 +1,16 @@
+import "../styles.css";
+
+const clock = document.querySelector("#clock");
+
+function getClock(){
+    const date = new Date();
+
+    const hours = String(date.getHours()).padStart(2, "0");
+    const mins = String(date.getMinutes()).padStart(2, "0");
+    const secs = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerHTML = `${hours}:${mins}:${secs}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
---
+++ src/js/login.js
@@ -0,0 +1,32 @@
+import "../styles.css";
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const intro = document.querySelector("#helloUser");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+function onLoginSubmit(e){
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    showHello(userName);
+}
+
+function showHello(userName){
+    intro.innerHTML = `Hello, ${userName}`;
+    intro.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if(savedUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else{
+    showHello(savedUserName);
+}
